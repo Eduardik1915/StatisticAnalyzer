e3407e230e9215bbb49f4ac6ce45619cff004155
Eduards Smulko <Eduardik1915@gmail.com>
2022-03-09 18:16:32 +0200

    V1.1

--- src/Starter.java
+++ src/Starter.java
@@ -9,7 +9,20 @@ public class Starter {
         Scanner scanner = new Scanner(System.in);
         System.out.println("В ведите путь к файлу, который необходимо зашифровать или расшифровать");
         System.out.print("Путь к файлу: ");
-        Path pathToTheFile = Path.of("C:\\Users\\esmulko\\IdeaProjects\\Kriptoanalizator\\sometext.txt");
+        Path pathToTheFile = Path.of("C:/Users/esmulko/IdeaProjects/Kriptoanalizator/sometext.txt");
+       // Path pathToTheFile = Path.of(scanner.nextLine());
+        System.out.println("enter key");
+        int key = scanner.nextInt();
+
+   /*     System.out.println("Зашифровать файл => В ведите 1\nРасшифровать файл => В ведите 2\nВыйты из программы => В ведите 3");
+        System.out.println("");
+        if (scanner.nextInt() == 1) {
+            System.out.println("В ведите ключ: от -25 до 25");
+            int key = scanner.nextInt();
+        } else if (scanner.nextInt() == 2){
+            System.out.println("Тут пока пусто");
+        }*/
+
         //file creating
         String parentPath = String.valueOf(pathToTheFile.getParent());
         String fileName = String.valueOf(pathToTheFile.getFileName());
@@ -25,7 +38,7 @@ public class Starter {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(newPath.toFile()))
         ) {
             while (reader.ready()){
-                writer.write(reader.read()+1);
+                writer.write(reader.read()+key);
             }
         }
 
